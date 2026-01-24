a361640dee07075c898de02105e91e8f3d9045d0
Benjamin Salomon <benjaminsalomon699@gmail.com>
2026-01-24 23:05:57 +0300

    Day 02 exercises

---
+++ src/day02/exercise01.kt
@@ -0,0 +1,14 @@
+package day02
+
+// EXERCISE
+fun main() {
+    val language = "Kotlin"
+    var level = 1
+
+    println(language)
+    println(level)
+
+    level = 2
+
+    println(level)
+}
---
+++ src/day02/exercise02.kt
@@ -0,0 +1,14 @@
+package day02
+
+// ASSIGNMENT
+fun main() {
+    val username = "benjamin"
+    val email = "benjaminsalomo@example.com"
+    var loginCount = 0
+
+    loginCount = 1
+
+    println(username)
+    println(email)
+    println(loginCount)
+}
